d64c8c7a0a5cb236e45dfcd0e449c5850400768b
Praise Obah <obahsomto@gmail.com>
2024-03-03 07:31:21 -0800

    html 1

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+	const amenityObj = {};
+	$('.amenities .popover input').change(function () {
+		if ($(this).is(':checked')) {
+			amenityObj[$(this).attr('data-name')] = $(this).attr('data-id');
+		} else if ($(this).is(':not(:checked)')) {
+			delete amenityObj[$(this).attr('data-name')];
+		}
+		const names = Object.keys(amenityObj);
+		$('.amentities h4').text(names.sort().join(', '));
+	});
+}
